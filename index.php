867f22e66d5d7fdb2d52b4c83ddaec8459fbd93c
Turag3819 <kawsaruddin1955@gmail.com>
2021-10-20 13:03:43 +0600

    problem-2 solved

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php
+
+/**Problem 2 */
+
 $n = 5;
 for ($i = 1; $i <= $n; $i++) {
   for ($j = 1; $j <= $n; $j++) {
-    if($j == $i || $j+$i ==6){
+    if ($j == $i || $j + $i == 6) {
       echo '*';
-    }else{
+    } else {
       echo '&nbsp;&nbsp;&nbsp;';
     }
   }
   echo '<br>';
-
 }
-  echo "<hr>";
+echo "<hr>";
+/*******************************
+            Problem 2 
+ *******************************   
+   Input- aAcDgbE
+   Output- AaCdGBe
+          Capital to small : 3
+          Small to capital : 4 
       
-?>
-
-<!-- *                                            3 
-  ***                                           2
- *****                                          1
-*******                                         0 -->
+ *******************************/
+$input = ['a', 'A', 'c', 'D', 'g', 'b', 'E'];
+for ($i = 0; $i < count($input); $i++) {
+  if (ctype_upper($input[$i])) {
+    $input[$i] = strtolower($input[$i]);
+    $captal_to_small[] = $input[$i];
+  } else {
+    $input[$i] = strtoupper($input[$i]);
+    $small_to_capital[] = $input[$i];
+  }
+}
+$implode = implode('', $input);
+echo 'Output = '.$implode.'<br> <br>';
+echo "Capital To Small = ".count($captal_to_small).'<br> <br>';
+echo "Small To Capital = ".count($small_to_capital).'<br> <br>';
